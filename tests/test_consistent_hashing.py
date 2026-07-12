5015e1a08fc26da039613c7e500591cd7b682e17
JoePallister <joepallister@gmail.com>
2026-07-12 12:43:57 +0100

    testing_store_works_for_arbitrary_node_placement

--- tests/test_consistent_hashing.py
+++ tests/test_consistent_hashing.py
@@ -2,25 +2,32 @@ from app_consistent_hashing import (
     find_node,
     reassign_keys_and_delete_node,
     construct_hash_ring,
+    store_item,
+    Item,
+    hash,
 )
+
+
+def find_node_name_naive(key: str, hash_ring_size: int, ring: list) -> int:
+    hash_value = hash(key) % hash_ring_size
+    while hash_value not in [p for p, _ in ring]:
+        hash_value = (hash_value + 1) % hash_ring_size
+
+    return next(node for p, node in ring if p == hash_value)
 
 
 def test_find_node():
     test_key = "test_key"
     hash_ring_size = 9
-    test_ring = [(0, "node1"), (3, "node2"), (6, "node3")]
-    # Hash gives 5 for this key, so it should map to node3
-    assert find_node(test_key, hash_ring_size, test_ring) == "node3"
-
-
-# def test_find_node():
-#     test_key = "test_key"
-#     hash_ring_size = 9
-#     num_nodes = 3
-#     node_names = [f"node_{i}" for i in range(num_nodes)]
-#     test_ring = construct_hash_ring(node_names, hash_ring_size)
-#     # Hash gives 5 for this key, so it should map to node3
-#     assert find_node(test_key, hash_ring_size, test_ring) == "node3"
+    num_nodes = 3
+    node_names = [f"node_{i}" for i in range(num_nodes)]
+    test_ring = construct_hash_ring(node_names, hash_ring_size)
+    test_stores = {node_name: {} for node_name in node_names}
+    store_item(
+        Item(key=test_key, value="test_value"), stores=test_stores, ring=test_ring
+    )
+    expected_node = find_node_name_naive(test_key, hash_ring_size, test_ring)
+    assert find_node(test_key, hash_ring_size, test_ring) == expected_node
 
 
 def test_find_node_wrap_around():
